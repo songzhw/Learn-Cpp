c4dcaeac7798a0a32628a5b87986dd53fc37f6f4
songzhw <songzhw2012@gmail.com>
2017-07-03 13:44:21 -0400

    hello world (cpp)

--- Cpp101/main.cpp
+++ Cpp101/main.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include "Square.h"
+
 using namespace std;
 
 typedef int meter;
@@ -6,5 +8,8 @@ typedef int meter;
 int main() {
     meter distance = 5;
     cout << "Hello, World! " << distance << std::endl; //=> Hello, World! 5
+
+    Square square(20);
+    cout << "area = "<<square.getArea() << std::endl;  //=> 400; --> Destory class
     return 0;
 }
