3ab8385f311ed53144026e607f93c9647c7484f7
songzhw <songzhw2012@gmail.com>
2017-09-24 02:14:36 -0400

    C++ file

--- Cpp101/main.cpp
+++ Cpp101/main.cpp
@@ -1,19 +1,14 @@
-#include <iostream>
-#include <vector>
-#include <map>
-#include <fstream>
-#include <cmath>
-#include <sysdir.h>
-#include "Square.h"
+#include <iostream>  // 读文件
+#include <fstream>   // 读写文件
 
 using namespace std;
 
 int main() {
     ofstream ofs;
-    ofs.open("news.txt", ios::out|ios::in);
+    ofs.open("news.txt", ios::out|ios::in|ios::trunc); // if not exist, ios::trunc will create a new one
 
     ofs << "Brand New Content"<<endl;
     ofs << "Second Line"<<endl;
 
-    ofs.close();
+    ofs.close(); // 生成在"%Cpp101%/cmake-build-debug目录下"
 }
