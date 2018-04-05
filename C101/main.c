043c8cca843547a1757909c2761e1d6e4af96e45
songzhw <songzhw2012@gmail.com>
2018-04-04 21:43:17 -0400

    p++

--- C101/main.c
+++ C101/main.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 
-int main() {
+
+
+
+int pointer101() {
     int b = 55;
     printf("%d, %d\n", b, *&b); //=> 55, 55
     printf("%p, 0x%p\n", &b, &b); //=> 0x7ffee13abf68, 0x0x7ffee13abf68
@@ -15,6 +18,39 @@ int main() {
     a = 66;
     printf("%p %p %d \n", p, &a, *p); //=> 0x7ffee4446f3c 0x7ffee4446f3c 66
 
-
     return 0;
+}
+
+int main(){ // c中声明数据不是用 double[] aryF;
+    double aryD[3] = {1.1, 2.5, 4.0};
+    double* pD = aryD;
+    printf("size of a pointer is %d\n", sizeof(pD)); // size of a pointer is 8
+    printf("size of a doble is %d\n", sizeof(double)); // size of a doble is 8
+
+    printf("(01) %p || %f\n", pD, (*pD));  // (01) 0x7ffeef7d9f50 || 1.100000
+    pD = pD + 1;
+    printf("(02) %p || %f\n", pD, (*pD));  // (02) 0x7ffeef7d9f58 || 2.500000 (p++后, 果然是加了8)
+
+
+    int aryI[3] = {1, 3, 7};
+    int* pI = aryI;
+
+    printf("size of a pointer is %d\n", sizeof(pI)); // size of a pointer is 8
+    printf("size of a int is %d\n", sizeof(int)); // size of a int is 4
+
+    printf("(03) %p || %d\n", pI, (*pI));  // (03) 0x7ffee2e40f44 || 1
+    pI = pI + 1;
+    printf("(04) %p || %d\n", pI, (*pI));  // (04) 0x7ffee2e40f48 || 3 (这时p++后, 就只移4位)
+
+/*
+size of a pointer is 8
+size of a doble is 8
+(01) 0x7ffeed98ef50 || 1.100000
+(02) 0x7ffeed98ef58 || 2.500000
+
+ size of a pointer is 8
+size of a doble is 4
+(03) 0x7ffeed98ef44 || 1
+(04) 0x7ffeed98ef48 || 3
+ */
 }
