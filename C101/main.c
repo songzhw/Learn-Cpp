864606f5a9bafccfbe80eda50fe484d7fabdd7e8
songzhw <songzhw2012@gmail.com>
2018-04-08 17:50:23 -0400

    solo-file structure

--- C101/main.c
+++ C101/main.c
@@ -1,18 +1,14 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 
-void swap( int, int); // 函数原型的声明
+#define NUM 100.0
+
+double percent(int);
 
 void main() {
-    int a = 55, b = 66;
-    swap(a, b);
-    printf(" In main: a =% d, b =% d\n", a, b);
+    double percetage = percent(85);
+    printf(" In main: result = %f \n", percetage); //=> 0.850000
 }
 
-void swap(int a, int b) {
-    int temp = a;
-    a = b;
-    b = temp;
-    printf(" In swap: a =% d, b =% d\n", a, b);
+double percent(int arg) {
+    return arg / NUM;
 }
