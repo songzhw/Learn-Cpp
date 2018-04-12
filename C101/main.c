02caa5652f7b3f05c8e6749129f213adb8daf1e0
songzhw <songzhw2012@gmail.com>
2018-04-11 20:11:07 -0400

    method return value

--- C101/main.c
+++ C101/main.c
@@ -14,14 +14,14 @@ int main() {
     printf("\n"); //=> 1 3 5 (可预测)
     p = sp(a);
     for (i = 0; i < 3; i++) printf("% d ", *(p + i));
-    printf("\n"); //=> 3 0 0 (不可预测)
+    printf("\n"); //=> 3 7 1 (可预测)
     return 0;
 }
 
 int *sp(int s[]) {
-    int b[3];
+    static int b[3];
     b[0] = 2 + s[0];
     b[1] = 4 + s[1];
-    b[2] = 6 - s[2] + b[1];
+    b[2] = 6 - s[2];
     return b;
 }
