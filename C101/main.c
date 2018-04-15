4c43da50cd2896e947acc5ee338cc7ed94ae25d6
songzhw <songzhw2012@gmail.com>
2018-04-15 09:54:02 -0400

    rand

--- C101/main.c
+++ C101/main.c
@@ -12,22 +12,12 @@ int main() {
 
 #include <stdlib.h>
 #include <stdio.h>
+#include <time.h>
 
 int main() {
-    int num1 = atoi("230");     //=> 230
-    int num2 = atoi("240 a");   //=> 240
-    int num3 = atoi("ab250 d"); //=> 0
-    int num4 = atoi("260a3d");  //=> 260
-    printf("%d, %d, %d, %d\n", num1, num2, num3, num4);
-
-    char nums[] = "2001 60c0c0 -1101110100110100100000 0x6fffff";
-    char *pEnd;
-    long int li1, li2, li3, li4;
-    li1 = strtol(nums, &pEnd, 10);   //=> 2001
-    li2 = strtol(pEnd, &pEnd, 16);   //=> 6340800
-    li3 = strtol(pEnd, &pEnd, 2);    //=> -3624224
-    li4 = strtol(pEnd, NULL, 0);     //=> 7340031
-    printf("转换成10进制后: %ld、%ld、%ld、%ld\n", li1, li2, li3, li4);
+    srand((unsigned int) time(NULL));
+    printf("%d, %d, %d\n", rand(), rand(), rand());
+    printf("%d, %d, %d\n", rand()%4, rand()%4, rand()%4);
 
 
     return 0;
