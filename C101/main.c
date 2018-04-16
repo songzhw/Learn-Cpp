0b6abe4bb561d6f36a16dcd6f7607020bc8e6708
songzhw <songzhw2012@gmail.com>
2018-04-16 16:35:09 -0400

    time

--- C101/main.c
+++ C101/main.c
@@ -15,12 +15,22 @@ int main() {
 #include <time.h>
 
 int main() {
+
+
+
+    time_t timep;
+    time (&timep);
+    printf("%s \n", asctime(gmtime(&timep)));    //=> Mon Apr 16 20:29:26 2018
+    printf("%s \n", asctime(localtime(&timep))); //=> Mon Apr 16 16:29:26 2018
+    printf("%s \n", ctime(&timep));              //=> Mon Apr 16 16:29:26 2018
+
     time_t time1 = time(NULL);
     printf("%d\n", time1); //=> 1523909831
 
     time_t* p = malloc(sizeof(time_t));
     time_t time2 = time(p);
     printf("%d, %d, %p\n", time2, *p, p); //=> 1523909945, 1523909945, 0x7f93825006e0
+//    printf("%s \n", asctime(p));
     free(p);
 
     return 0;
