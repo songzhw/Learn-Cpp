a474b55f22de9d497b1c69b0d8d9b47132bfbd76
songzhw <songzhw2012@gmail.com>
2018-05-03 07:54:32 -0400

    string traps

--- C_Trap/StringTrap.c
+++ C_Trap/StringTrap.c
@@ -1,10 +1,14 @@
 
 #include "StringTrap.h"
 #include <stdio.h>
+#include <stdlib.h>
 
 void appendString(char* s1, char* s2) {
-    char result[100];
+//    char result[100];
+    int len = strlen(s1) + strlen(s2) + 1; //注意, 这里得+1, 因为要放结尾符'\0'
+    char* result = (char*) malloc(len);
     strcpy(result, s1);
     strcat(result, s2);
     printf("append result = %s \n", result);
+    free(result);
 }
