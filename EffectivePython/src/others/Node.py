9cbc7a6d40965410ebdf94c8c763cf596eb73e5a
songzhw <songzhw2012@gmail.com>
2018-06-05 17:40:17 -0400

    calculate 24

---
+++ EffectivePython/src/others/Node.py
@@ -0,0 +1,10 @@
+class Node(object):
+    def __init__(self, result = None):
+        self.result = result
+
+    def __repr__(self):
+        pass
+
+    def setExpression(self, left, right, operator):
+        return self
+
